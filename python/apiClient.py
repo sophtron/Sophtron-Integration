3f160eb1bca7d74fbf8f7ebf569baa559d74e30e
jishun <jishun@sophtron.com>
2022-04-14 00:57:47 +1000

    simplify python example

--- python/apiClient.py
+++ python/apiClient.py
@@ -1,7 +1,20 @@
 import os
 import json
 import requests
-from .auth import sophtron_auth_code
+import base64
+import hmac
+import hashlib
+
+def sophtron_auth_code(user_id: str, access_key: str, url: str, http_method: str):
+    auth_path = url[url.rfind('/'):].lower()
+    plain_key = '{}\n{}'.format(http_method.upper(), auth_path)
+    access_key_bytes = base64.b64decode(access_key)
+    signature = hmac.new(key=access_key_bytes,
+                         msg=bytes(plain_key, 'ascii'),
+                         digestmod=hashlib.sha256).digest()
+    sig_b64_str = base64.b64encode(signature).decode('ascii')
+    auth_str = 'FIApiAUTH:{}:{}:{}'.format(user_id, sig_b64_str, auth_path)
+    return auth_str
 
 
 class SophtronApiClient(object):
